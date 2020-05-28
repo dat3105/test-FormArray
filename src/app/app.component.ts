3bcbce0108e9cd40694a1cd5036ca391e79c08da
dat3105 <raiphantome@gmail.com>
2020-05-28 20:27:39 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-FormArray';
+  form: FormGroup;
+   
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      name: [],
+      address: fb.array([])
+    })
+  }
+
+  addNewAddressGroup() {
+    const add = this.form.get('address') as FormArray;
+    add.push(this.fb.group({
+      street: [],
+      city: []
+    }))
+  }
+
+  deleteAddressGroup(index: number) {
+    const add = this.form.get('address') as FormArray;
+    add.removeAt(index)
+  }
 }
